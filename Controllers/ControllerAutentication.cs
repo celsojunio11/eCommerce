39c2c05bafbf5a8f588dfe66d3529d8a8bf6dd98
celsojunio11 <celsojunio11@gmail.com>
2021-11-08 10:55:10 -0300

    autentication

--- Controllers/ControllerAutentication.cs
+++ Controllers/ControllerAutentication.cs
@@ -10,155 +10,156 @@ using eCommerce.Models.Table;
 
 namespace eCommerce.Controllers
 {
-    public class AutenticationController : Controller
-    {
-        private readonly eCommerceContext _context;
+      public class AutenticationController : Controller
+      {
+            private readonly eCommerceContext _context;
 
-        public AutenticationController(eCommerceContext context)
-        {
-            _context = context;
-        }
-
-        // GET: Autentication
-        public async Task<IActionResult> Index()
-        {
-            var Autentications = await _context.Autentications.Include("IdPessoaNavigation.PessoaFisica")
-                .Include("IdPessoaNavigation.PessoaJuridica")
-                .ToListAsync();
-            return View(Autentications);
-        }
-
-        // GET: Autentication/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            public AutenticationController(eCommerceContext context)
             {
-                return NotFound();
+                  _context = context;
             }
 
-            var Autentication = await _context.Autentications
-                .Include(c => c.IdPessoaNavigation)
-                .FirstOrDefaultAsync(m => m.IdPessoa == id);
-            if (Autentication == null)
+            // GET: Autentication
+            public async Task<IActionResult> Index()
             {
-                return NotFound();
+                  var Autentications = await _context.Autentications.Include("IdPessoaNavigation.PessoaFisica")
+                      .Include("IdPessoaNavigation.PessoaJuridica")
+                      .ToListAsync();
+                  return View(Autentications);
             }
 
-            return View(Autentication);
-        }
-
-        // GET: Autentication/Create
-        public IActionResult Create()
-        {
-            ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome");
-            return View();
-        }
-
-        // POST: Autentication/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdPessoa,IsPreferencial")] Autentication Autentication)
-        {
-            if (ModelState.IsValid)
+            // GET: Autentication/Details/5
+            public async Task<IActionResult> Details(int? id)
             {
-                _context.Add(Autentication);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome", Autentication.IdPessoa);
-            return View(Autentication);
-        }
-
-        // GET: Autentication/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var Autentication = await _context.Autentications.Include(c => c.IdPessoaNavigation.PessoaFisica)
-                .Include(c => c.IdPessoaNavigation.PessoaJuridica)
-                .SingleAsync(c => c.IdPessoa == id);
-            if (Autentication == null)
-            {
-                return NotFound();
-            }
-            // ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome", Autentication.IdPessoa);
-            return View(Autentication);
-        }
-
-        // POST: Autentication/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdPessoa, IsPreferencial, IdPessoaNavigation,")] Autentication Autentication)
-        {
-            if (id != Autentication.IdPessoa)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(Autentication);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AutenticationExists(Autentication.IdPessoa))
-                    {
+                  if (id == null)
+                  {
                         return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome", Autentication.IdPessoa);
-            return View(Autentication);
-        }
+                  }
 
-        // GET: Autentication/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
+                  var Autentication = await _context.Autentications
+                      .Include(c => c.IdPessoaNavigation)
+                      .FirstOrDefaultAsync(m => m.IdPessoa == id);
+                  if (Autentication == null)
+                  {
+                        return NotFound();
+
+                  }
+
+                  return View(Autentication);
+            }
+
+            // GET: Autentication/Create
+            public IActionResult Create()
             {
-                return NotFound();
+                  ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome");
+                  return View();
             }
 
-            var Autentication = await _context.Autentications
-                .Include(c => c.IdPessoaNavigation)
-                .FirstOrDefaultAsync(m => m.IdPessoa == id);
-            if (Autentication == null)
+            // POST: Autentication/Create
+            // To protect from overposting attacks, enable the specific properties you want to bind to.
+            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Create([Bind("IdPessoa,IsPreferencial")] Autentication Autentication)
             {
-                return NotFound();
+                  if (ModelState.IsValid)
+                  {
+                        _context.Add(Autentication);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                  }
+                  ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoaa", "Nome", Autentication.IdPessoa);
+                  return View(Autentication);
             }
 
-            return View(Autentication);
-        }
+            // GET: Autentication/Edit/5
+            public async Task<IActionResult> Edit(int? id)
+            {
+                  if (id == null)
+                  {
+                        return NotFound();
+                  }
 
-        // POST: Autentication/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var Autentication = await _context.Autentications.FindAsync(id);
-            _context.Autentications.Remove(Autentication);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+                  var Autentication = await _context.Autentications.Include(c => c.IdPessoaNavigation.PessoaFisica)
+                      .Include(c => c.IdPessoaNavigation.PessoaJuridica)
+                      .SingleAsync(c => c.IdPessoa == id);
+                  if (Autentication == null)
+                  {
+                        return NotFound();
+                  }
+                  // ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome", Autentication.IdPessoa);
+                  return View(Autentication);
+            }
 
-        private bool AutenticationExists(int id)
-        {
-            return _context.Autentications.Any(e => e.IdPessoa == id);
-        }
-    }
+            // POST: Autentication/Edit/5
+            // To protect from overposting attacks, enable the specific properties you want to bind to.
+            // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Edit(int id, [Bind("IdPessoa, IsPreferencial, IdPessoaNavigation,")] Autentication Autentication)
+            {
+                  if (id != Autentication.IdPessoa)
+                  {
+                        return NotFound();
+                  }
+
+                  if (ModelState.IsValid)
+                  {
+                        try
+                        {
+                              _context.Update(Autentication);
+                              await _context.SaveChangesAsync();
+                        }
+                        catch (DbUpdateConcurrencyException)
+                        {
+                              if (!AutenticationExists(Autentication.IdPessoa))
+                              {
+                                    return NotFound();
+                              }
+                              else
+                              {
+                                    throw;
+                              }
+                        }
+                        return RedirectToAction(nameof(Index));
+                  }
+                  ViewData["IdPessoa"] = new SelectList(_context.Pessoas, "IdPessoa", "Nome", Autentication.IdPessoa);
+                  return View(Autentication);
+            }
+
+            // GET: Autentication/Delete/5
+            public async Task<IActionResult> Delete(int? id)
+            {
+                  if (id == null)
+                  {
+                        return NotFound();
+                  }
+
+                  var Autentication = await _context.Autentications
+                      .Include(c => c.IdPessoaNavigation)
+                      .FirstOrDefaultAsync(m => m.IdPessoa == id);
+                  if (Autentication == null)
+                  {
+                        return NotFound();
+                  }
+
+                  return View(Autentication);
+            }
+
+            // POST: Autentication/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> DeleteConfirmed(int id)
+            {
+                  var Autentication = await _context.Autentications.FindAsync(id);
+                  _context.Autentications.Remove(Autentication);
+                  await _context.SaveChangesAsync();
+                  return RedirectToAction(nameof(Index));
+            }
+
+            private bool AutenticationExists(int id)
+            {
+                  return _context.Autentications.Any(e => e.IdPessoa == id);
+            }
+      }
 }
